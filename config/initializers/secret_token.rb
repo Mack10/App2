91dd33d95db0c4513d1c9ec0518da29fceefcee2
Mack10 <info@ryanparentphoto.com>
2012-02-02 23:46:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2::Application.config.secret_token = '3c791691849e9af666f622bab2035acabcfb6f92fdeca04b8d18e4e2356c46752e893f2b44d99f70dccb48e742be333176ddb26e601b79f9d23439f1073a930a'
